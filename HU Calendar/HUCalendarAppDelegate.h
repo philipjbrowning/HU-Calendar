1c7a53251ec46b122eafa65509c7e42d1ee36525
Philip Browning <philipjbrowning@gmail.com>
2012-11-07 19:01:50 -0500

    Initial Commit

---
+++ HU Calendar/HUCalendarAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HUCalendarAppDelegate.h
+//  HU Calendar
+//
+//  Created by Philip J Browning II on 11/7/12.
+//  Copyright (c) 2012 Howard University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HUCalendarAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HU Calendar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HU Calendar
+//
+//  Created by Philip J Browning II on 11/7/12.
+//  Copyright (c) 2012 Howard University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HUCalendarAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HUCalendarAppDelegate class]));
+    }
+}
